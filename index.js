1365bf8860f01ad40b42cddacae09b6c197066cb
mosfiqurbd <moshfiqurrahim2018@gmail.com>
2024-11-05 17:23:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const menuIcon = document.getElementById
+("menu-icon");
+const navMenu = document.getElementById
+("menu");
+menuIcon.addEventListener("Click", ()==>{
+ if (navMenu.className==="hidden") {
+      navMenu?.classList.remove("hidden");
+     
+    } else{
+        navMenu?.classList.add("hidden");
+
+    }
+ }); 
+    
